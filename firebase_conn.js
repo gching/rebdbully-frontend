a53b63135f200abdec156340e7cb128fd5560e82
Jimicy <jimmy_wang1995@hotmail.com>
2015-09-12 19:47:58 -0400

    Matt's code for firebase is weird.

---
+++ firebase_conn.js
@@ -0,0 +1,181 @@
+/**
+* Firebase API Class
+* ------------------------
+* Enables some simple functions to get and set from the firebase API
+* Will fetch single or all videos stored on cloudinary by fetching the URL location stored in Firebase
+* Will find documents for videos stored on firebase
+*/
+
+/**
+ *
+ * Videos 
+ *    src
+ *    title
+ *    [List of Documents]
+ *        Document
+ *            owner
+ *            [List of Notes]
+ *                Note
+ *                    message
+ *                    subject
+ * 
+ */
+
+
+// Constructor
+function FirebaseConn(){
+  // The property used to actually contain the reference to Firebase
+  // Connects to Specific Firebase DB
+  this.dbRefVids = new Firebase('https://qh9wny0cxht.firebaseio-demo.com/videos');
+}
+
+
+
+/**
+ * Getters
+ * --------------------------------
+ */
+
+// Retrieve a specific video by ID
+// Requires the key value for the video
+// Requires a callback
+FirebaseConn.prototype.getVideo = function (videoKey,callback){
+  this.dbRefVids.orderByKey().equalTo(videoKey).limitToFirst(1).on('child_added',function(data){
+    console.log("getVideo",data.key(),data.val());
+    callback(data.key(),data.val());
+  }); 
+
+};
+
+// Fetches all Videos
+// Requires a callback function
+FirebaseConn.prototype.getVideos = function(callback){
+  this.dbRefVids.on('child_added',function(data){
+    callback(data.key(),data.val());
+    console.log("getVideos",data.key(),data.val());
+  });
+}
+
+// Retrieve a specific document by ID
+// Requires a the key value representing the video, as well as the key name for the document
+// Requires a callback
+FirebaseConn.prototype.getDocument = function(videoKey,documentKey,callback){
+  this.dbRefVids.orderByKey().equalTo(videoKey).child(documentKey).on("child_added",function(data){
+    console.log(data.key(),data.val());
+    callback(data.key(),data.val());
+  });
+}
+
+// Fetch all Documents
+// Requires the key value for the video
+// Requires a callback
+FirebaseConn.prototype.getDocuments = function(videoKey,callback){
+  this.dbRefVids.orderByKey().equalTo(videoKey).on('child_added',function(data){
+    console.log("getDocuments",data.key(),data.val());
+    callback(data.key(),data.val());
+  });
+}
+
+// Retrieve a specific document by ID
+// Requires a the key value representing the video, as well as the key name for the document
+// Requires a callback
+FirebaseConn.prototype.getSection = function(videoKey,documentKey,sectionKey,callback){
+  var videoRef = this.dbRefVids.child(videoKey);
+  var docRef = videoRef.child("docs").child(documentKey);
+  var section = docRef.child("sections").child(sectionKey).on("child_added",function(data){
+    console.log(data.key(),data.val());
+    callback(data.key(),data.val());
+  });
+}
+
+FirebaseConn.prototype.getSections = function(videoKey,documentKey,callback){
+  var videoRef = this.dbRefVids.child(videoKey);
+  var docRef = videoRef.child("docs").child(documentKey);
+  var sections = docRef.child("sections").on("child_added",function(data){
+    console.log("getSections",data.key(),data.val());
+    callback(data.key(),data.val());
+  });
+}
+
+
+/**
+ * Setters
+ * -------------------------------
+ */
+
+FirebaseConn.prototype.setSection = function(note,title,videoKey,documentKey){
+  // Create a new Document
+  var videoRef = this.dbRefVids.child(videoKey);
+  var docRef = videoRef.child("docs").child(documentKey);
+  var section = docRef.child("sections").push({
+    note: note,
+    title: title
+  });
+
+  var sectionKey = section.key();
+
+  console.log(sectionKey);
+}
+
+// Create new document, or update
+FirebaseConn.prototype.setDocument = function(owner,videoKey,documentKey){
+  if (videoKey === null || videoKey === ''){
+    throw "ERROR: Cannot have undefined video key";
+  }
+
+  // By default, assume it's a new document being saved
+  documentKey = documentKey || null;
+
+  // Checks!
+
+  // TODO: Find parameters and do checks
+
+  // Update the Document Specific Information
+/*  if (documentKey !=null ){
+    var videoRef = this.dbRefVids.child(videoKey);
+    var documentRef = videoRef.child("docs");
+    documentRef.update({
+      notes: notes
+    });
+    return;
+  }*/
+
+  // Create a new Document
+  var videoRef = this.dbRefVids.child(videoKey);
+  var newDocument = videoRef.child("docs").push({
+    owner: owner
+  });
+
+  var newDocumentKey = newDocument.key();
+
+  console.log(newDocumentKey);
+
+}
+
+
+// Create new Video, or update
+FirebaseConn.prototype.setVideo = function(title,fileLoc,videoKey){
+  // By default, assume it's a new video being saved
+  videoKey = videoKey || null;
+
+  // Checks!
+
+  // TODO: Find parameters and do checks
+
+  // By default, assume it's a new video being saved
+/*  if (videoKey !=null ){
+    var videoRef = this.dbRefVids.child(videoKey);
+    videoRef.update({
+      title: title
+    });
+    return;
+  }*/
+
+  var newVideo = this.dbRefVids.push({
+    title: title,
+    src: fileLoc
+  });
+
+  var newVideoKey = newVideo.key();
+}
+
--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var playerInstance = jwplayer('myElement');
 
+var videoKey = "http://www.youtube.com/watch?v=u4rizXqsmdc";
+
 playerInstance.setup({
-  file: 'pageinfo-close-window.mp4',
+  file: "http://www.youtube.com/watch?v=u4rizXqsmdc",
   width: 1000,
   height: 600
 });
@@ -24,156 +26,108 @@ playerInstance.on('complete', function(event) {
   console.log(duration);
 });
 
-
-
 //---------------------------------------------------
 
-  //// Initialize Firebase.
-  var firepadRef = getExampleRef();
-  // TODO: Replace above line with:
-  // var firepadRef = new Firebase('<YOUR FIREBASE URL>');
-  //// Create CodeMirror (with lineWrapping on).
-  var codeMirror = CodeMirror(document.getElementById('firepad-container'), { lineWrapping: true });
-  //// Create Firepad (with rich text toolbar and shortcuts enabled).
-  var firepad = Firepad.fromCodeMirror(firepadRef, codeMirror,
-      { richTextToolbar: true, richTextShortcuts: true });
-  //// Initialize contents.
-  firepad.on('ready', function() {
-    if (firepad.isHistoryEmpty()) {
-      firepad.setHtml(
-            '<header-timestamp>Header Element</header-timestamp>'
-          );
-    }
+  var fcon = new FirebaseConn();
+  fcon.getDocuments(videoKey, function(key, value) {
+    console.log("getDocuments");
+    console.log(key, value);
   });
 
-  // Button click to create header with timestamp
-  function buttonClick() {
-    console.log(duration[duration.length-1]);
-    firepad.makeHeaderDialog_(duration[duration.length-1]);
-  }
-  // An example of a complex custom entity.
 
-  firepad.registerEntity('header-timestamp', {
+  // var firepadRef = setDocument()
+  // // TODO: Replace above line with:
+  // // var firepadRef = new Firebase('<YOUR FIREBASE URL>');
+  // //// Create CodeMirror (with lineWrapping on).
+  // var codeMirror = CodeMirror(document.getElementById('firepad-container'), { lineWrapping: true });
+  // //// Create Firepad (with rich text toolbar and shortcuts enabled).
+  // var firepad = Firepad.fromCodeMirror(firepadRef, codeMirror,
+  //     { richTextToolbar: true, richTextShortcuts: true });
+  // //// Initialize contents.
+  // firepad.on('ready', function() {
+  //   if (firepad.isHistoryEmpty()) {
+  //     firepad.setHtml(
+  //           '<header-timestamp>Header Element</header-timestamp>'
+  //         );
+  //   }
+  // });
 
-    /**
-    Renders the element given the info with listener on click.
-    */
-    render: function(info, entityHandler) {
-      console.log('render')
-      console.log(entityHandler)
+  // // Button click to create header with timestamp
+  // function buttonClick() {
+  //   console.log(duration[duration.length-1]);
+  //   firepad.makeHeaderDialog_(duration[duration.length-1]);
+  // }
+  // // An example of a complex custom entity.
 
-      // Create a header with h2 tag
-      var headerWithTimestamp = document.createElement('span');
-      headerWithTimestamp.style['font-size'] = '24px';
+  // firepad.registerEntity('header-timestamp', {
 
-      // Set the timestamp on the element
-      if (info.timestamp){
-        headerWithTimestamp.timestamp = info.timestamp;
-      }
+  //   /**
+  //   Renders the element given the info with listener on click.
+  //   */
+  //   render: function(info, entityHandler) {
+  //     console.log('render')
+  //     console.log(entityHandler)
 
-      // Set the text content from the information
-      headerWithTimestamp.textContent = info.textContent;
+  //     // Create a header with h2 tag
+  //     var headerWithTimestamp = document.createElement('span');
+  //     headerWithTimestamp.style['font-size'] = '24px';
+
+  //     // Set the timestamp on the element
+  //     if (info.timestamp){
+  //       headerWithTimestamp.timestamp = info.timestamp;
+  //     }
+
+  //     // Set the text content from the information
+  //     headerWithTimestamp.textContent = info.textContent;
 
 
-      // When clicked, return information on what the time is
-      headerWithTimestamp.addEventListener('click', function(){
-        console.log('Clicked with timestamp ' + info.timestamp);
-      });
+  //     // When clicked, return information on what the time is
+  //     headerWithTimestamp.addEventListener('click', function(){
+  //       console.log('Clicked with timestamp ' + info.timestamp);
+  //     });
 
-      console.log(headerWithTimestamp)
-      return headerWithTimestamp;
+  //     console.log(headerWithTimestamp);
+  //     return headerWithTimestamp;
 
-    }.bind(this),
+  //   }.bind(this),
 
-    /**
-    Sets default info with timestamp and the actual text inside from the
-    actual element inserted.
-    */
-    fromElement: function(element){
-      // Sets a timestamp or a provided with from the element from the
-      // provided attribute. As well, gets the text in between the tag and
-      // provides it in info.
-      // TODO - Set the timestamp attribute
-      var info = {timestamp: '0:00', textContent: element.textContent};
-      return info;
-    },
-    /**
-    On update from firebase, we update it on the element.
-    */
-    update: function(info, element) {
-      // TODO - Change the text if changed.
-      if (info.timestamp){
-        element.timestamp = info.timestamp
-      } else {
-        element.timestamp = '0:00'
-      }
-    }
-    /**
-    Possibly exports to firebase and saves it this way
-    TODO - Fallbacks to render so don't need it
-    */
-    /*
-    export: function (info) {
+  //   /**
+  //   Sets default info with timestamp and the actual text inside from the
+  //   actual element inserted.
+  //   */
+  //   fromElement: function(element){
+  //     // Sets a timestamp or a provided with from the element from the
+  //     // provided attribute. As well, gets the text in between the tag and
+  //     // provides it in info.
+  //     // TODO - Set the timestamp attribute
+  //     var info = {timestamp: '0:00', textContent: element.textContent};
+  //     return info;
+  //   },
+  //   /**
+  //   On update from firebase, we update it on the element.
+  //   */
+  //   update: function(info, element) {
+  //     // TODO - Change the text if changed.
+  //     if (info.timestamp){
+  //       element.timestamp = info.timestamp
+  //     } else {
+  //       element.timestamp = '0:00'
+  //     }
+  //   }
+  //   /**
+  //   Possibly exports to firebase and saves it this way
+  //   TODO - Fallbacks to render so don't need it
+  //   */
+  //   /*
+  //   export: function (info) {
 
-      var inputElement = document.createElement('checkbox');
-      if(info.checked) {
-        inputElement.setAttribute('checked', true);
-      }
-      console.log("blah" + inputElement)
-      return inputElement;
-    }
-    */
+  //     var inputElement = document.createElement('checkbox');
+  //     if(info.checked) {
+  //       inputElement.setAttribute('checked', true);
+  //     }
+  //     console.log("blah" + inputElement)
+  //     return inputElement;
+  //   }
+  //   */
 
-  });
-  firepad.registerEntity('checkbox', {
-    render: function (info, entityHandler) {
-      var inputElement = document.createElement('input');
-      inputElement.setAttribute('type', 'checkbox');
-      if(info.checked) {
-        inputElement.checked = 'checked';
-      }
-      inputElement.addEventListener('click', function () {
-        entityHandler.replace({checked:this.checked});
-      });
-      return inputElement;
-    }.bind(this),
-    fromElement: function (element) {
-      var info = {};
-      if(element.hasAttribute('checked')) {
-        info.checked = true;
-      }
-      return info;
-    },
-    update: function (info, element) {
-      console.log(info)
-      console.log('here')
-      if (info.checked) {
-        element.checked = 'checked';
-      } else {
-        element.checked = null;
-      }
-    },
-    export: function (info) {
-      var inputElement = document.createElement('checkbox');
-      if(info.checked) {
-        inputElement.setAttribute('checked', true);
-      }
-      console.log("blah" + inputElement)
-      return inputElement;
-    }
-  });
-
-  // Helper to get hash from end of URL or generate a random one.
-  function getExampleRef() {
-  var ref = new Firebase('https://radiant-fire-7825.firebaseio.com/');
-  var hash = window.location.hash.replace(/#/g, '');
-  if (hash) {
-    ref = ref.child(hash);
-  } else {
-    ref = ref.push(); // generate unique location.
-    window.location = window.location + '#' + ref.key(); // add it as a hash to the URL.
-  }
-  if (typeof console !== 'undefined')
-    console.log('Firebase data: ', ref.toString());
-  return ref;
-}
+  // });
